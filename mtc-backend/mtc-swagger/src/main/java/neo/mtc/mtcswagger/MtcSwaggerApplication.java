8c989c4518174668d99fbeb749f808ac42366763
mayureshRewale <mayureshrewale@gmail.com>
2023-03-19 14:30:49 +0530

    Adedd all modules to mtc-backend

---
+++ mtc-backend/mtc-swagger/src/main/java/neo/mtc/mtcswagger/MtcSwaggerApplication.java
@@ -0,0 +1,13 @@
+package neo.mtc.mtcswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtcSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtcSwaggerApplication.class, args);
+	}
+
+}
---
+++ mtc-backend/mtc-swagger/src/test/java/neo/mtc/mtcswagger/MtcSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package neo.mtc.mtcswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtcSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
